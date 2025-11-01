2205dbc1addf244a503743d70af9aa09953bbc08
elhannnn <agit02394@gmail.com>
2025-11-01 09:20:45 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace SbnApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Model/BuyerMaster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SbnApplication.Model
+{
+    public class BuyerMaster
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } //isi sesuai form
+
+        public string Description { get; set; }
+        public string BillingAddress { get; set;
+
+    }
+}
